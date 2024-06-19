267f49de56bad515307ae11deb3ac0e95f07d593
Eyad ahmed <Eyad932004@outloo.com>
2024-06-19 09:53:46 +0300

    #[0002]-finish setup constants files & add all needed images

---
+++ src/utils/index.ts
@@ -0,0 +1,39 @@
+import logo from "../assets/img/logo.png";
+
+export const logoImg = logo;
+
+import render_1 from "../assets/img/Render-1.png";
+import render_2 from "../assets/img/Render-2.png";
+import render_3 from "../assets/img/Render-3.png";
+import render_4 from "../assets/img/Render-4.png";
+
+export const bg_1 = render_1;
+export const bg_2 = render_2;
+export const bg_3 = render_3;
+export const bg_4 = render_4;
+
+import person_1 from "../assets/img/person_1.png";
+import person_2 from "../assets/img/person_2.png";
+import person_3 from "../assets/img/person_3.png";
+
+export const crew_m_1 = person_1;
+export const crew_m_2 = person_2;
+export const crew_m_3 = person_3;
+
+import des_a from "../assets/img/moon.png";
+import des_b from "../assets/img/mars.png";
+import des_c from "../assets/img/europa.png";
+import des_d from "../assets/img/titan.png";
+
+export const moon = des_a;
+export const mars = des_b;
+export const europa = des_c;
+export const titan = des_d;
+
+import tech_1 from "../assets/img/launch.png";
+import tech_2 from "../assets/img/port.png";
+import tech_3 from "../assets/img/capsule.png";
+
+export const launch = tech_1;
+export const port = tech_2;
+export const capsule = tech_3;
