b449cbbc054765fc0307fd369e9530d355c9b831
Eyad ahmed <Eyad932004@outloo.com>
2024-06-23 10:00:31 +0300

    #[0016]-setup the Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Home } from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="h-full">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
