409eddc9764131375cb2d256e75c32f7380c8534
Eyad ahmed <Eyad932004@outloo.com>
2024-06-24 18:29:25 +0300

    #[0018]-edit destination data objects & edit a to prop in navLinks objects

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const navLinks = [
   { id: "00", title: "home", to: "/" },
   { id: "01", title: "destination", to: "/destination" },
   { id: "02", title: "crew", to: "/crew" },
-  { id: "03", title: "technology", to: "technology" },
+  { id: "03", title: "technology", to: "/technology" },
 ];
 
 import {
@@ -44,6 +44,7 @@ export const crewData = [
 
 export const destinationsData = [
   {
+    id: "1",
     name: "moon",
     description:
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
@@ -52,6 +53,7 @@ export const destinationsData = [
     img: moon,
   },
   {
+    id: "2",
     name: "mars",
     description:
       "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
@@ -60,6 +62,7 @@ export const destinationsData = [
     img: mars,
   },
   {
+    id: "3",
     name: "europa",
     description:
       "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
@@ -68,6 +71,7 @@ export const destinationsData = [
     img: europa,
   },
   {
+    id: "4",
     name: "titan",
     description:
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
