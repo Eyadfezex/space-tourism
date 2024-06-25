fafb402e0b965a332f12d94346e643262f67e328
Eyad ahmed <Eyad932004@outloo.com>
2024-06-25 07:56:33 +0300

    #[0026]-install Loading component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Destination } from "./pages/Destination";
 import { Home } from "./pages/Home";
+import { Loading } from "./components/loading-screen/Loading";
 import { Routes, Route } from "react-router-dom";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  useEffect(() => {
+    location.pathname !== "/" ? (window.location.href = "/") : "";
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, [location.pathname]);
+
   return (
     <div className="h-full">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
+        <Route index element={loading ? <Loading /> : <Home />} />
+
+        <Route
+          path="/destination"
+          element={loading ? <Loading /> : <Destination />}
+        />
       </Routes>
     </div>
   );
