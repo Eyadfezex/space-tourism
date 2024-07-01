b65550411007b612ac1246b7bbac074cb8d9ca47
Eyad ahmed <Eyad932004@outloo.com>
2024-07-01 18:53:52 +0300

    #[0041]-fix-loading screen problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import { Destination } from "./pages/Destination";
 import { Home } from "./pages/Home";
 import { Loading } from "./components/loading-screen/Loading";
@@ -11,37 +9,22 @@ import { NavBar } from "./components/navBar/NavBar";
 import { SideBarProvider } from "./context/SideBarContext";
 import { Tech } from "./pages/Tech";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const location = useLocation();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1400);
-  }, [location.pathname]);
-  useEffect(() => {
-    setLoading(true);
-  }, [location.pathname]);
-
   return (
-    <div className="h-full">
-      {loading ? (
+    <div>
+      <>
         <Loading />
-      ) : (
-        <>
-          <SideBarProvider>
-            <SideBar />
-            <NavBar />
-          </SideBarProvider>
+        <SideBarProvider>
+          <SideBar />
+          <NavBar />
+        </SideBarProvider>
 
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/destination" element={<Destination />} />
-            <Route path="/crew" element={<Crew />} />
-            <Route path="/technology" element={<Tech />} />
-          </Routes>
-        </>
-      )}
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Tech />} />
+        </Routes>
+      </>
     </div>
   );
 }
--- src/components/loading-screen/Loading.tsx
+++ src/components/loading-screen/Loading.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { PropagateLoader } from "react-spinners";
 export const Loading = () => {
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1400);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    setLoading(true);
+  }, [location.pathname]);
+
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-navy">
+    <div
+      className={`absolute z-20 flex items-center justify-center w-full h-screen bg-navy ${loading ? "block" : "hidden"}`}
+    >
       <PropagateLoader color="white" />
     </div>
   );
